0c94811f1e97b78ef6afab8d381afffeb93fc6df
Sm0keyyyy <145635076+Sm0keyyyy@users.noreply.github.com>
2023-09-26 01:08:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]");
+      });
+    }
+  });
+
+  /* Sticky navbar */
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /* Remove toggle icon and navbar when click navbar link (scroll)*/
+  menuIcon.classList.toggle("bx-x");
+  menuIcon.classList.toggle("active");
+};
+
+ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .skill-container, .project-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/* Typed Js */
+var typing = new Typed(".typing", {
+  strings: [
+    "Clown",
+    "Programmer",
+    "Student",
+    "Guitarist",
+    "Junior Web Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+/* mobile responsive navbar Js */
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
